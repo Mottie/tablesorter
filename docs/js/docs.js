b67fb88032626e0cfada70b10f5460510ad8b12e
Rob Garrison <wowmotty@gmail.com>
2012-05-08 13:13:21 -0500

    updated docs

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -35,11 +35,23 @@ $(function(){
 		return false;
 	});
 
+	var animating = false;
+
 	$('.collapsible').hide();
-	$('.toggle2').click(function(){
-		$(this).closest('tr').find('.collapsible').slideToggle();
-		return false;
-	});
+	$('.toggle2')
+		.click(function(e){
+			if (!animating) {
+				animating = true;
+				$(this).closest('tr').find('.collapsible').slideToggle();
+				setTimeout(function(){ animating = false; }, 200);
+			}
+			return false;
+		});
+	$('.toggle2, span.permalink')
+		.dblclick(function(){
+			window.location.hash = '#' + $(this).closest('tr')[0].id;
+			return false;
+		});
 
 	$('.toggleAll, .showAll, .hideAll').click(function(){
 		var t = $.trim($(this).text());
@@ -55,8 +67,31 @@ $(function(){
 
 });
 
+function showProperty(){
+	var prop, h = window.location.hash;
+	if (h) {
+		var prop = $(h);
+		if (prop.length) {
+			prop.find('.collapsible').show();
+			if (h === '#csschildrow') {
+				$('#root .expand-child').show();
+			}
+			// move below sticky header
+			if (/options/.test(prop.closest('table')[0].id)) {
+				$(window).scrollTop( prop.position().top - 30 );
+			}
+		}
+	}
+};
+
 $(window).load(function(){
 	$(".js").chili();
 	$(".html").chili();
 	$(".css").chili();
+
+	$(window).bind('hashchange', function(){
+		showProperty();
+	});
+	showProperty();
+
 });
