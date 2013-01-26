610ee0442cecefa2c46eadb61515c3964226d833
Mottie <wowmotty@gmail.com>
2013-01-26 09:21:13 -0600

    replaced chili with prettify (syntax highlighting)

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,31 +1,19 @@
-/* Stop IE flicker */
-if ($.browser.msie) { document.execCommand('BackgroundImageCache', false, true); }
-
-jQuery.fn.antispam = function() {
-	return this.each(function(){
-		var email = $(this).text().toLowerCase().replace(/\sdot/g,'.').replace(/\sat/g,'@').replace(/\s+/g,''),
-			URI = "mailto:" + email;
-		$(this).hide().before(
-			$("<a></a>").attr("href",URI).addClass("external").text(email)
-		);
-	});
-};
-
 $(function(){
 
 	$("a.external").each(function(){this.target = '_new';});
-	$("span.email").antispam();
 
 	// get javascript source
 	if ($("#js").length) {
 		$("#javascript pre").text( $("#js").html().replace(/\t/g, "  ") );
 	}
 	if ($("#css").length) {
-		$("pre.css").text( $("#css").html().replace(/\t/g, "  ") );
+		$("pre.lang-css").text( $("#css").html().replace(/\t/g, "  ") );
 	}
 	if ($("#demo").length) {
 		$("#html pre").text( $("#demo").html().replace(/\t/g, "  ") );
 	}
+
+	if (typeof prettyPrint !== 'undefined') { prettyPrint(); }
 
 	// hide child rows
 	$('#root .tablesorter-childRow').hide();
@@ -85,9 +73,6 @@ function showProperty(){
 }
 
 $(window).load(function(){
-	$(".js").chili();
-	$(".html").chili();
-	$(".css").chili();
 
 	$(window).bind('hashchange', function(){
 		showProperty();
