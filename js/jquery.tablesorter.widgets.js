0dc7fded5052986eb37867e90806e0cad9d2f94c
Mottie <wowmotty@gmail.com>
2013-12-08 14:52:13 -0600

    Add stickyHeaders_attachTo widget option. Fixes #295 & #314

--- js/jquery.tablesorter.widgets.js
+++ js/jquery.tablesorter.widgets.js
@@ -1054,6 +1054,7 @@ ts.addWidget({
 	priority: 60, // sticky widget must be initialized after the filter widget!
 	options: {
 		stickyHeaders : '',       // extra class name added to the sticky header row
+		stickyHeaders_attachTo : null, // jQuery selector or object to attach sticky header to
 		stickyHeaders_offset : 0, // number or jquery selector targeting the position:fixed element
 		stickyHeaders_cloneId : '-sticky', // added to table ID, if it exists
 		stickyHeaders_addResizeEvent : true, // trigger "resize" event on headers
@@ -1064,21 +1065,24 @@ ts.addWidget({
 		if (c.$table.hasClass('hasStickyHeaders')) { return; }
 		var $cell,
 			$table = c.$table,
-			$win = $(window),
+			$attach = $(wo.stickyHeaders_attachTo),
 			$thead = $table.children('thead:first'),
+			$win = $attach.length ? $attach : $(window),
 			$header = $thead.children('tr').not('.sticky-false').children(),
 			innerHeader = '.tablesorter-header-inner',
 			$tfoot = $table.find('tfoot'),
 			filterInputs = '.tablesorter-filter',
 			$stickyOffset = isNaN(wo.stickyHeaders_offset) ? $(wo.stickyHeaders_offset) : '',
-			stickyOffset = $stickyOffset.length ? $stickyOffset.height() || 0 : parseInt(wo.stickyHeaders_offset, 10) || 0,
+			stickyOffset = $attach.length ? 0 : $stickyOffset.length ?
+				$stickyOffset.height() || 0 : parseInt(wo.stickyHeaders_offset, 10) || 0,
 			$stickyTable = wo.$sticky = $table.clone()
 				.addClass('containsStickyHeaders')
 				.css({
-					position   : 'fixed',
+					position   : $attach.length ? 'absolute' : 'fixed',
 					margin     : 0,
 					top        : stickyOffset,
-					visibility : 'hidden',
+					left       : 0,
+					visibility : $attach.length ? 'visible' : 'hidden',
 					zIndex     : wo.stickyHeaders_zIndex ? wo.stickyHeaders_zIndex : 2
 				}),
 			$stickyThead = $stickyTable.children('thead:first').addClass('tablesorter-stickyHeader ' + wo.stickyHeaders),
@@ -1097,7 +1101,9 @@ ts.addWidget({
 					spacing = parseInt($header.eq(0).css('border-left-width'), 10) * 2;
 				}
 				$stickyTable.css({
-					left : $thead.offset().left - $win.scrollLeft() - spacing,
+					left : $attach.length ? parseInt($table.css('padding-left'), 10) +
+						parseInt($table.css('margin-left'), 10) + parseInt($table.css('border-left-width'), 10) :
+						$thead.offset().left - $win.scrollLeft() - spacing,
 					width: $table.width()
 				});
 				$stickyCells.filter(':visible').each(function(i) {
@@ -1166,23 +1172,27 @@ ts.addWidget({
 			});
 		// add stickyheaders AFTER the table. If the table is selected by ID, the original one (first) will be returned.
 		$table.after( $stickyTable );
+
 		// make it sticky!
 		$win.bind('scroll.tsSticky resize.tsSticky', function(event) {
 			if (!$table.is(':visible')) { return; } // fixes #278
 			var prefix = 'tablesorter-sticky-',
 				offset = $table.offset(),
 				captionHeight = (wo.stickyHeaders_includeCaption ? 0 : $table.find('caption').outerHeight(true)),
-				scrollTop = $win.scrollTop() + stickyOffset - captionHeight,
+				scrollTop = ($attach.length ? $attach.offset().top : $win.scrollTop()) + stickyOffset - captionHeight,
 				tableHeight = $table.height() - ($stickyTable.height() + ($tfoot.height() || 0)),
-				isVisible = (scrollTop > offset.top) && (scrollTop < offset.top + tableHeight) ? 'visible' : 'hidden';
+				isVisible = (scrollTop > offset.top) && (scrollTop < offset.top + tableHeight) ? 'visible' : 'hidden',
+				cssSettings = { visibility : isVisible };
+			if ($attach.length) {
+				cssSettings.top = $attach.scrollTop();
+			} else {
+				// adjust when scrolling horizontally - fixes issue #143
+				cssSettings.left = $thead.offset().left - $win.scrollLeft() - spacing;
+			}
 			$stickyTable
 				.removeClass(prefix + 'visible ' + prefix + 'hidden')
 				.addClass(prefix + isVisible)
-				.css({
-					// adjust when scrolling horizontally - fixes issue #143
-					left : $thead.offset().left - $win.scrollLeft() - spacing,
-					visibility : isVisible
-				});
+				.css(cssSettings);
 			if (isVisible !== laststate || event.type === 'resize') {
 				// make sure the column widths match
 				resizeHeader();
