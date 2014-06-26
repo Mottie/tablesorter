3b4cd54c615d0b8ba387634ff63c0ac4774cc7d7
Mottie <wowmotty@gmail.com>
2014-06-26 12:09:59 -0500

    Scroller: add scroller_upAfterSort option. Fixes #660

--- js/widgets/widget-scroller.js
+++ js/widgets/widget-scroller.js
@@ -73,6 +73,7 @@ ts.addWidget({
 		scroller_height : 300,
 		scroller_barWidth : 18,
 		scroller_jumpToHeader: true,
+		scroller_upAfterSort: true,
 		scroller_idPrefix : 's_'
 	},
 	init: function(table, thisWidget, c, wo){
@@ -198,7 +199,9 @@ ts.addWidget({
 		}
 
 		// Sorting, so scroll to top
-		$tbl.parent().animate({ scrollTop: 0 }, 'fast');
+		if (wo.scroller_upAfterSort) {
+			$tbl.parent().animate({ scrollTop: 0 }, 'fast');
+		}
 
 	},
 	remove : function(table, c, wo){
