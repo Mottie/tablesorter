33b3abe75afb512d015f0297ea41e30faa45cd86
Mottie <wowmotty@gmail.com>
2014-09-15 19:14:50 -0500

    Parser: Add duration parser & demo

--- js/parsers/parser-duration.js
+++ js/parsers/parser-duration.js
@@ -4,8 +4,6 @@
 ;(function($){
 "use strict";
 
-	$.tablesorter.defaults.durationLabels = 'y,d,h,m,s';
-
 	// If any number > 9999, then set table.config.durationLength = 5
 	// The below regex matches this duration example: 1y 23d 12h 44m 9s
 	$.tablesorter.addParser({
@@ -20,20 +18,20 @@
 				duration = '',
 				len = c.durationLength || 4,
 				str = new Array(len + 1).join('0'),
-				labels = (c.durationLabels || 'y,d,h,m,s').split(/\s*,\s*/),
+				labels = (c.durationLabels || '(?:years|year|y),(?:days|day|d),(?:hours|hour|h),(?:minutes|minute|min|m),(?:seconds|second|sec|s)').split(/\s*,\s*/),
 				llen = labels.length;
-
+			// build regex
 			if (!c.durationRegex) {
 				for (i = 0; i < llen; i++) {
-					t += '(?:(\\d+)' + labels[i] + '\\s*)?';
+					t += '(?:(\\d+)\\s*' + labels[i] + '\\s*)?';
 				}
 				c.durationRegex = new RegExp(t, 'i');
 			}
-			time = s.match(c.durationRegex);
+			// remove commas from value
+			time = ( c.usNumberFormat ? s.replace(/,/g, '') : s.replace( /(\d)(?:\.|\s*)(\d)/g, '$1$2') ).match(c.durationRegex);
 			for (i = 1; i < llen + 1; i++) {
 				duration += ( str + ( time[i] || 0 ) ).slice(-len);
 			}
-console.log(duration, s, time);
 			return duration;
 		},
 		type: "text"
