7fa790795369385615206c4489e35f7fb7ef445f
Rob Garrison <wowmotty@gmail.com>
2015-10-26 10:56:01 -0500

    Parser: Globalize parser now allows a different language per column

--- js/parsers/parser-globalize.js
+++ js/parsers/parser-globalize.js
@@ -1,24 +1,27 @@
-/*! Parser: jQuery Globalize - updated 5/17/2015 (v2.22.0) */
+/*! Parser: jQuery Globalize - updated 10/26/2015 (v2.23.6) */
 /* Extract localized data using jQuery's Globalize parsers; set
- Globalize.locale( 'xx' ) prior to initializing tablesorter! */
+ Globalize.locale( 'xx' ) in the globalize settings */
 /*jshint jquery:true */
 /*global Globalize:false */
 ;( function( $ ) {
 	'use strict';
 
 	/*! jQuery Globalize date parser (https://github.com/jquery/globalize#date-module) */
-	/* demo: http://jsfiddle.net/Mottie/0j18Lw8r/ */
 	$.tablesorter.addParser({
 		id: 'globalize-date',
 		is: function () {
 			return false;
 		},
 		format: function ( str, table, cell, cellIndex ) {
-			var c = table.config,
-			// add options to 'config.globalize' for all columns --> globalize : { skeleton: 'GyMMMd' }
-			// or per column by using the column index --> globalize : { 0 : { datetime: 'medium' } }
-			options = c.globalize && ( c.globalize[ cellIndex ] || c.globalize ) || {},
-			date = Globalize && Globalize.dateParser ? Globalize.dateParser( options )( str ) :
+			var globalize, date,
+				c = table.config,
+				// add options to 'config.globalize' for all columns --> globalize : { skeleton: 'GyMMMd' }
+				// or per column by using the column index --> globalize : { 0 : { datetime: 'medium' } }
+				options = c.globalize && ( c.globalize[ cellIndex ] || c.globalize ) || {};
+			if ( Globalize ) {
+				globalize = typeof options.lang === 'object' ? options.lang : Globalize( options.lang || 'en' );
+			}
+			date = globalize && globalize.dateParser ? globalize.dateParser( options )( str ) :
 				str ? new Date( str ) : str;
 			return date instanceof Date && isFinite( date ) ? date.getTime() : str;
 		},
@@ -26,18 +29,21 @@
 	});
 
 	/*! jQuery Globalize number parser (https://github.com/jquery/globalize#number-module) */
-	/* demo: http://jsfiddle.net/Mottie/0j18Lw8r/ */
 	$.tablesorter.addParser({
 		id: 'globalize-number',
 		is: function () {
 			return false;
 		},
 		format: function ( str, table, cell, cellIndex ) {
-			var c = table.config,
-			// add options to 'config.globalize' for all columns --> globalize : { skeleton: 'GyMMMd' }
-			// or per column by using the column index --> globalize : { 0 : { datetime: 'medium' } }
-			options = c.globalize && ( c.globalize[ cellIndex ] || c.globalize ) || {},
-			num = Globalize && Globalize.numberParser ? Globalize.numberParser( options )( str ) :
+			var globalize, num,
+				c = table.config,
+				// add options to 'config.globalize' for all columns --> globalize : { skeleton: 'GyMMMd' }
+				// or per column by using the column index --> globalize : { 0 : { datetime: 'medium' } }
+				options = c.globalize && ( c.globalize[ cellIndex ] || c.globalize ) || {};
+			if ( Globalize ) {
+				globalize = typeof options.lang === 'object' ? options.lang : Globalize( options.lang || 'en' );
+			}
+			num = globalize && globalize.numberParser ? globalize.numberParser( options )( str ) :
 				str ? $.tablesorter.formatFloat( ( str || '' ).replace( /[^\w,. \-()]/g, '' ), table ) : str;
 			return str && typeof num === 'number' ? num : str;
 		},
