b31f786caa58850dc9fd465c3f6d300c4bf5ca8a
Rob Garrison <wowmotty@gmail.com>
2015-10-31 10:07:27 -0500

    Sort2Hash: update to include pager & filter parameters

--- js/widgets/widget-sort2Hash.js
+++ js/widgets/widget-sort2Hash.js
@@ -1,44 +1,61 @@
-/*! Widget: sort2Hash - updated 7/28/2015 (v2.22.4) */
+/*! Widget: sort2Hash - updated 10/30/2015 (v2.23.6) */
+/* Requires tablesorter v2.8+ and jQuery 1.7+
+ * by Rob Garrison
+ */
 ;( function( $ ) {
 	'use strict';
 	var ts = $.tablesorter || {},
 	s2h = {
 		init : function( c, wo ) {
-			var hasSaveSort = ts.hasWidget( c.table, 'saveSort' ),
-				sort = s2h.getSort( c, wo );
+			var filter, temp, page, size,
+				table = c.table,
+				pager = c.pager,
+				hasSaveSort = ts.hasWidget( table, 'saveSort' ),
+				sort = s2h.decodeHash( c, wo, 'sort' );
 			if ( ( sort && !hasSaveSort ) || ( sort && hasSaveSort && wo.sort2Hash_overrideSaveSort ) ) {
-				s2h.processHash( c, wo, sort );
+				s2h.convertString2Sort( c, wo, sort );
 			}
-			c.$table.on( 'sortEnd.sort2hash', function() {
-				s2h.setHash( c, wo );
+			if ( ts.hasWidget( c.table, 'pager' ) ) {
+				temp = parseInt( s2h.decodeHash( c, wo, 'page' ), 10 );
+				page = pager.page = ( temp < 0 ? 0 : ( temp > pager.totalPages ? pager.totalPages - 1 : temp ) ) + 1;
+				size = pager.size = parseInt( s2h.decodeHash( c, wo, 'size' ), 10 );
+			}
+			if ( ts.hasWidget( table, 'filter' ) ) {
+				filter = s2h.decodeHash( c, wo, 'filter' );
+				if ( filter ) {
+					filter = filter.split( wo.sort2Hash_separator );
+					c.$table.one( 'tablesorter-ready', function() {
+						setTimeout(function(){
+							c.$table.one( 'filterEnd', function(){
+								$(this).trigger( 'pageAndSize', [ page, size ] );
+							});
+							$.tablesorter.setFilters( table, filter, true );
+						}, 100 );
+					});
+				}
+			} else {
+				c.$table.trigger( 'pageAndSize', [ page, size ] );
+			}
+			c.$table.on( 'sortEnd.sort2hash filterEnd.sort2hash pagerComplete.sort2Hash', function() {
+				if ( this.hasInitialized ) {
+					s2h.setHash( this.config, this.config.widgetOptions );
+				}
 			});
 		},
+
 		getTableId : function( c, wo ) {
 			// option > table id > table index on page
 			return wo.sort2Hash_tableId ||
 				c.table.id ||
 				'table' + $( 'table' ).index( c.$table );
 		},
-		getSort : function( c, wo, clean ) {
-			// modified original code from http://www.netlobo.com/url_query_string_javascript.html
-			var value,
-				name = s2h.getTableId( c, wo ).replace( /[\[]/, '\\[' ).replace( /[\]]/, '\\]' ),
-				sort = ( new RegExp( '[\\#&]' + name + '=([^&]*)' ) ).exec( window.location.hash );
-			if ( sort === null ) {
-				return '';
-			} else {
-				value = s2h.processSort( c, wo, sort[ 1 ] );
-				if ( clean ) {
-					window.location.hash = window.location.hash.replace( '&' + name + '=' + sort[ 1 ], '' );
-					return value;
-				}
-				return sort[ 1 ];
-			}
+		regexEscape : function( v ) {
+			return v.replace( /([\.\^\$\*\+\-\?\(\)\[\]\{\}\\\|])/g, '\\$1');
 		},
 		// convert 'first%20name,asc,last%20name,desc' into [[0,0], [1,1]]
-		processHash : function( c, wo, sortHash ) {
-			var regex, column, direction, temp,
-				arry = decodeURI( sortHash || '' ).split( wo.sort2Hash_separator ),
+		convertString2Sort : function( c, wo, sortHash ) {
+			var regex, column, direction, temp, index, $cell,
+				arry = sortHash.split( wo.sort2Hash_separator ),
 				indx = 0,
 				len = arry.length,
 				sort = [];
@@ -49,14 +66,18 @@
 				// ignore wo.sort2Hash_useHeaderText setting &
 				// just see if column contains a number
 				if ( isNaN( temp ) || temp > c.columns ) {
-					regex = new RegExp( '(' + column + ')', 'i' );
-					column = c.$headers.filter( function( index ) {
-						return regex.test( c.$headers[ index ].textContent || '' );
-					}).attr( 'data-column' );
+					regex = new RegExp( '(' + s2h.regexEscape( column ) + ')', 'i' );
+					for ( index = 0; index < c.columns; index++ ) {
+						$cell = c.$headerIndexed[ index ];
+						if ( regex.test( $cell.attr( wo.sort2Hash_headerTextAttr ) ) ) {
+							column = index;
+							index = c.columns;
+						}
+					}
 				}
 				direction = arry[ indx++ ];
 				// ignore unpaired values
-				if ( typeof direction !== 'undefined' ) {
+				if ( typeof column !== 'undefined' && typeof direction !== 'undefined' ) {
 					// convert text to 0, 1
 					if ( isNaN( direction ) ) {
 						// default to ascending sort
@@ -71,56 +92,138 @@
 		},
 
 		// convert [[0,0],[1,1]] to 'first%20name,asc,last%20name,desc'
-		processSort : function( c, wo ) {
+		convertSort2String : function( c, wo ) {
 			var index, txt, column, direction,
 				sort = [],
 				arry = c.sortList || [],
 				len = arry.length;
 			for ( index = 0; index < len; index++ ) {
 				column = arry[ index ][ 0 ];
-				if ( wo.sort2Hash_useHeaderText ) {
-					txt = $.trim( c.$headerIndexed[ column ].text() );
-					if ( typeof wo.sort2Hash_processHeaderText === 'function' ) {
-						txt = wo.sort2Hash_processHeaderText( txt, c, column );
-					}
-					column = txt;
-				}
-				sort.push( column );
+				txt = $.trim( c.$headerIndexed[ column ].attr( wo.sort2Hash_headerTextAttr ) );
+				sort.push( txt !== '' ? encodeURIComponent( txt ) : column );
 				direction = wo.sort2Hash_directionText[ arry[ index ][ 1 ] ];
 				sort.push( direction );
-
 			}
 			// join with separator
 			return sort.join( wo.sort2Hash_separator );
 		},
+
+		convertFilter2String : function( c, wo ) {
+			var index, txt, column, direction,
+				sort = [],
+				arry = c.sortList || [],
+				len = arry.length;
+			for ( index = 0; index < len; index++ ) {
+				column = arry[ index ][ 0 ];
+				txt = $.trim( c.$headerIndexed[ column ].attr( wo.sort2Hash_headerTextAttr ) );
+				column = typeof txt !== 'undefined' ? encodeURIComponent( txt ) : column;
+				sort.push( column );
+				direction = wo.sort2Hash_directionText[ arry[ index ][ 1 ] ];
+				sort.push( direction );
+			}
+			// join with separator
+			return sort.join( wo.sort2Hash_separator );
+		},
+
+		encodeHash : function( c, wo, component, value, rawValue ) {
+			var result = false,
+				tableId = s2h.getTableId( c, wo );
+			if ( typeof wo.sort2Hash_encodeHash === 'function' ) {
+				result = wo.sort2Hash_encodeHash( c, tableId, component, value, rawValue || value );
+			}
+			if ( result === false ) {
+				result = '&' + component + '[' + tableId + ']=' + value;
+			}
+			return result;
+		},
+
+		decodeHash : function( c, wo, component ) {
+			var regex,
+				result = false,
+				tableId = s2h.getTableId( c, wo );
+			if ( typeof wo.sort2Hash_decodeHash === 'function' ) {
+				result = wo.sort2Hash_decodeHash( c, tableId, component );
+			}
+			if ( result === false ) {
+				regex = new RegExp( '[\\#&]' + component + '\\[' + s2h.regexEscape( tableId ) + '\\]=([^&]*)' ),
+				/*jshint -W030 */
+				result = regex.exec( window.location.hash );
+			}
+			return result ? decodeURIComponent( result[ 1 ] ) : '';
+		},
+
+		cleanHash : function( c, wo, component, hash ) {
+			var index, len, parts, regex,
+				result = false,
+				tableId = s2h.getTableId( c, wo );
+			if ( typeof wo.sort2Hash_cleanHash === 'function' ) {
+				result = wo.sort2Hash_cleanHash( c, tableId, component, hash );
+			}
+			if ( result === false ) {
+				result = [];
+				parts = ( hash || '' ).slice(1).split( '&' );
+				len = parts.length;
+				regex = new RegExp( component + '\\[' + s2h.regexEscape( tableId ) + '\\]=([^&]*)' );
+				for ( index = 0; index < len; index++ ) {
+					if ( !regex.test( parts[ index ] ) ) {
+						result.push( parts[ index ] );
+					}
+				}
+			}
+			return result.length ? '#' + result.join( '&' ) : '';
+		},
+
 		setHash : function( c, wo ) {
-			var sort = s2h.processSort( c, wo );
-			if ( sort.length ) {
-				// remove old hash
-				s2h.getSort( c, wo, true );
-				window.location.hash += ( window.location.hash.length ? '' : wo.sort2Hash_hash ) +
-					 '&' + s2h.getTableId( c, wo ) + '=' + encodeURI( sort );
+			var str = '',
+				hash = window.location.hash,
+				hasPager = ts.hasWidget( c.table, 'pager' ),
+				hasFilter = ts.hasWidget( c.table, 'filter' ),
+				sortList = s2h.convertSort2String( c, wo ),
+				filters = ( hasFilter && c.lastSearch.join('') !== '' ? c.lastSearch : [] ),
+				filtersStr = encodeURIComponent( filters.join( c.widgetOptions.sort2Hash_separator ) ),
+				components = {
+					'sort'   : sortList ? s2h.encodeHash( c, wo, 'sort', sortList, c.sortList ) : '',
+					'page'   : hasPager ? s2h.encodeHash( c, wo, 'page', c.pager.page + 1 ) : '',
+					'size'   : hasPager ? s2h.encodeHash( c, wo, 'size', c.pager.size ) : '',
+					'filter' : filtersStr ? s2h.encodeHash( c, wo, 'filter', filtersStr, filters ) : ''
+				};
+			// remove old hash
+			$.each( components, function( component, value ) {
+				hash = s2h.cleanHash( c, wo, component, hash );
+				str += value;
+			});
+			// add updated hash
+			try {
+				window.history.pushState( null, null, window.location.pathname + wo.sort2Hash_hash + str );
+			} catch ( err ) {
+				window.location.hash = ( ( window.location.hash || '' ).replace( '#', '' ).length ? hash : wo.sort2Hash_hash ) + str;
 			}
 		}
 	};
 
 	ts.addWidget({
 		id: 'sort2Hash',
-		priority: 30, // after saveSort
+		priority: 60, // after saveSort & pager
 		options: {
 			sort2Hash_hash              : '#',      // hash prefix
 			sort2Hash_separator         : '-',      // don't '#' or '=' here
-			sort2Hash_tableId           : null,     // this option > table ID > table index on page,
-			sort2Hash_useHeaderText     : false,    // use column header text (true) or zero-based column index
-			sort2Hash_processHeaderText : null,     // function( text, config, columnIndex ) {},
+			sort2Hash_headerTextAttr    : 'data-header', // data attribute containing alternate header text
 			sort2Hash_directionText     : [ 0, 1 ], // [ 'asc', 'desc' ],
 			sort2Hash_overrideSaveSort  : false     // if true, override saveSort widget if saved sort available
+
+			// Options below commented out for improved compression
+			// ******************
+			// sort2Hash_tableId           : null,     // this option > table ID > table index on page,
+			// custom hash processing functions
+			// sort2Hash_encodeHash        : null,
+			// sort2Hash_decodeHash        : null,
+			// sort2Hash_cleanHash         : null
 		},
 		init: function(table, thisWidget, c, wo) {
 			s2h.init( c, wo );
 		},
 		remove: function(table, c) {
-			c.$table.off( 'sortEnd.sort2hash' );
+			c.$table.off( '.sort2hash' );
 		}
 	});
 
