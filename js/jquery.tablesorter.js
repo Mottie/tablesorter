001bcae65218b7f830f73a76afe7cd423b40bf7f
Rob Garrison <wowmotty@gmail.com>
2015-11-06 12:41:00 -0600

    Core: sortRestart works again with multi-row headers. Fixes #1074

--- js/jquery.tablesorter.js
+++ js/jquery.tablesorter.js
@@ -1402,13 +1402,12 @@
 				event[ c.sortResetKey ] ? 2 : ( c.sortVars[ col ].count + 1 ) % ( c.sortReset ? 3 : 2 );
 			// reset all sorts on non-current column - issue #30
 			if ( c.sortRestart ) {
-				tmp = cell;
 				for ( headerIndx = 0; headerIndx < len; headerIndx++ ) {
 					$header = c.$headers.eq( headerIndx );
+					tmp = parseInt( $header.attr( 'data-column' ), 10 );
 					// only reset counts on columns that weren't just clicked on and if not included in a multisort
-					if ( $header[ 0 ] !== tmp &&
-						( notMultiSort || $header.hasClass( ts.css.sortNone ) ) ) {
-						c.sortVars[ $header.attr( 'data-column' ) ].count = -1;
+					if ( col !== tmp && ( notMultiSort || $header.hasClass( ts.css.sortNone ) ) ) {
+						c.sortVars[ tmp ].count = -1;
 					}
 				}
 			}
