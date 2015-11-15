8a9e52827e62414d63cf5ee128faac78ba97abbd
Rob Garrison <wowmotty@gmail.com>
2015-11-14 19:27:27 -0600

    Parser: input-select updates. See #971
    - Prevent javascript error.
    - Include thead checkbox code to update tbody column checkboxes.
    - Add thead checkbox updating with indeterminate state.

--- js/parsers/parser-input-select.js
+++ js/parsers/parser-input-select.js
@@ -10,7 +10,8 @@
 		// do something here to update your server, if needed
 		// event = change event object
 		// $table = jQuery object of the table that was just updated
-		// $input = jQuery object of the input or select that was modified
+		// $input = jQuery object(s) of the input or select that was modified; in v2.24.6,
+		//   if the thead has a checkbox, $input may include multiple elements
 	};
 
 	// Custom parser for parsing input values
@@ -125,22 +126,20 @@
 	// you can change it to use delegate (v1.4.3+) or live (v1.3+) as desired
 	// if this code interferes somehow, target the specific table $('#mytable'), instead of $('table')
 	$( function() {
+		if ( !$.fn.on ) { return; }
 		$( 'table' ).on( 'tablesorter-initialized updateComplete', function() {
-			var namespace = '.parser-forms',
-			restoreValue = function( isTbody ) {
-				// make sure we restore original values (trigger blur)
-				// isTbody is needed to prevent the select from closing in IE
-				// see https://connect.microsoft.com/IE/feedbackdetail/view/962618/
-				if ( isTbody ) {
-					$( ':focus' ).blur();
-				}
-				return;
-			};
+			this.tablesorterBusy = false;
+			var namespace = '.parser-forms';
 			// bind to .tablesorter (default class name)
 			$( this ).children( 'tbody' )
 			.off( namespace )
 			.on( 'mouseleave' + namespace, function( event ) {
-				restoreValue( event.target.nodeName === 'TBODY' );
+				// make sure we restore original values (trigger blur)
+				// isTbody is needed to prevent the select from closing in IE
+				// see https://connect.microsoft.com/IE/feedbackdetail/view/962618/
+				if ( event.target.nodeName === 'TBODY' ) {
+					$( ':focus' ).blur();
+				}
 			})
 			.on( 'focus' + namespace, 'select, input, textarea', function() {
 				$( this ).data( 'ts-original-value', this.value );
@@ -162,28 +161,92 @@
 					( event.target.nodeName === 'INPUT' || event.target.nodeName === 'TEXTAREA' && event.altKey ) ) ) {
 					var undef,
 						$target = $( event.target ),
+						isCheckbox = event.target.type === 'checkbox',
 						$cell = $target.closest( 'td' ),
 						$table = $cell.closest( 'table' ),
 						indx = $cell[ 0 ].cellIndex,
 						c = $table[ 0 ].config || false,
+						busy = $table.length && $table[ 0 ].tablesorterBusy,
 						$hdr = c && c.$headerIndexed && c.$headerIndexed[ indx ] || [],
-						val = $target.val();
-					// abort if not a tablesorter table, or don't use updateCell if column is set
+						val = isCheckbox ? event.target.checked : $target.val();
+					// abort if not a tablesorter table, or busy, or don't use updateCell if column is set
 					// to 'sorter-false' and 'filter-false', or column is set to 'parser-false'
-					if ( $hdr.length && ( $hdr.hasClass( 'parser-false' ) ||
-						( $hdr.hasClass( 'sorter-false' ) && $hdr.hasClass( 'filter-false' ) ) ) ) {
+					if ( $.isEmptyObject( c ) || busy !== false || $hdr.length && ( $hdr.hasClass( 'parser-false' ) ||
+						( $hdr.hasClass( 'sorter-false' ) && $hdr.hasClass( 'filter-false' ) ) ) ||
+						// table already updating; get out of here, we might be in an endless loop (in IE)! See #971
+						( event.type === 'change' && c.table.isUpdating ) ) {
 						return;
 					}
 					// ignore change event if nothing changed
-					if ( c && val !== $target.data( 'ts-original-value' ) || event.target.type === 'checkbox' ) {
+					if ( c && val !== $target.data( 'ts-original-value' ) || isCheckbox ) {
 						$target.data( 'ts-original-value', val );
+						$table[ 0 ].tablesorterBusy = true;
 						// pass undefined resort value so it falls back to config.resort setting
 						$.tablesorter.updateCell( c, $cell, undef, function() {
 							updateServer( event, $table, $target );
+							$table[ 0 ].tablesorterBusy = false;
 						});
 					}
 				}
 			});
+
+			// add code for a checkbox in the header to set/unset all checkboxes in a column
+			if ( $( this ).children( 'thead' ).find( 'input[type="checkbox"]' ) ) {
+				$( this )
+				.off( namespace )
+				.on( 'tablesorter-ready' + namespace, function() {
+					var checkboxClass, $rows, len,
+						$table = $( this ),
+						c = $table.length && $table[ 0 ].config;
+					if ( !$.isEmptyObject( c ) ) {
+						this.tablesorterBusy = true;
+						checkboxClass = c && c.checkboxClass || 'checked';
+						$rows = $table.children( 'tbody' ).children( ':visible' ); // (include child rows?)
+						len = $rows.length;
+						// set indeterminate state on header checkbox
+						$( this ).children( 'thead' ).find( 'input[type="checkbox"]' ).each( function() {
+							var column = $( this ).closest( 'td, th' ).attr( 'data-column' ),
+								vis = $rows.filter( '.' + checkboxClass + '-' + column ).length,
+								allChecked = vis === len;
+							if ( vis === 0 || allChecked ) {
+								this.checked = allChecked;
+								this.indeterminate = false;
+							} else {
+								this.indeterminate = true;
+							}
+						});
+						this.tablesorterBusy = false;
+					}
+				})
+				.children( 'thead' )
+				.off( namespace )
+				// modified from http://jsfiddle.net/abkNM/6163/
+				.on( 'change' + namespace, 'input[type="checkbox"]', function( event ) {
+					var undef, onlyVisible, column, $target,
+						$checkbox = $( this ),
+						$table = $checkbox.closest( 'table' ),
+						c = $table.length && $table[ 0 ].config,
+						isChecked = this.checked;
+					if ( $table.length && c && !$table[ 0 ].tablesorterBusy ) {
+						column = parseInt( $checkbox.closest( 'td, th' ).attr( 'data-column' ), 10 );
+						onlyVisible = $table.length && c.checkboxVisible;
+						$table[ 0 ].tablesorterBusy = true; // prevent "change" event from calling updateCell numerous times (see #971)
+						$target = $table
+							.children( 'tbody' )
+							.children( 'tr' + ( typeof onlyVisible === 'undefined' || onlyVisible === true ? ':visible' : '' ) )
+							.children( ':nth-child(' + ( column + 1 ) + ')' )
+							.find( 'input[type="checkbox"]' )
+							.prop( 'checked', isChecked );
+						$.tablesorter.update( c, undef, function() {
+							updateServer( event, $table, $target );
+							$table[ 0 ].tablesorterBusy = false;
+						});
+					}
+					// update already going on, don't do anything!
+					return false;
+				});
+			}
+
 		});
 	});
 
