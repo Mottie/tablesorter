2cb01b4ec2678510d9084d577a393bf40a6b6055
Rob Garrison <wowmotty@gmail.com>
2015-11-14 22:17:29 -0600

    Docs: update utility option section

--- js/parsers/parser-image.js
+++ js/parsers/parser-image.js
@@ -1,5 +1,6 @@
 /*! Parser: image - new 7/17/2014 (v2.17.5) */
 /* alt attribute parser for jQuery 1.7+ & tablesorter 2.7.11+ */
+/* NOTE! Moved to jquery.tablesorter.js (core) in v2.18.0 */
 /*jshint jquery:true, unused:false */
 ;(function($){
 	'use strict';
