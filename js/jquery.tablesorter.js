e76e54098267ddfd9fd1a706e99774ae4d151c54
Rob Garrison <wowmotty@gmail.com>
2015-12-18 17:45:55 -0600

    Core: fix time parser detection. See #1107

--- js/jquery.tablesorter.js
+++ js/jquery.tablesorter.js
@@ -2624,7 +2624,7 @@
 	});
 
 	// match 24 hour time & 12 hours time + am/pm - see http://regexr.com/3c3tk
-	ts.regex.timeTest = /^([1-9]|1[0-2]):([0-5]\d)(\s[AP]M)|((?:[01]\d|[2][0-4]):[0-5]\d)$/i;
+	ts.regex.timeTest = /^([1-9]|1[0-2]):([0-5]\d)(\s[AP]M)$|^((?:[01]\d|[2][0-4]):[0-5]\d)$/i;
 	ts.regex.timeMatch = /([1-9]|1[0-2]):([0-5]\d)(\s[AP]M)|((?:[01]\d|[2][0-4]):[0-5]\d)/i;
 	ts.addParser({
 		id : 'time',
