eaec5db0f48d783a66bf85c06659313ad5aee5ff
Rob Garrison <wowmotty@gmail.com>
2017-07-05 13:44:27 -0500

    Output: bump version & add min

--- js/widgets/widget-output.js
+++ js/widgets/widget-output.js
@@ -1,4 +1,4 @@
-/*! Widget: output - updated 4/2/2017 (v2.28.6) *//*
+/*! Widget: output - updated 7/5/2017 (v2.28.16) *//*
  * Requires tablesorter v2.8+ and jQuery 1.7+
  * Modified from:
  * HTML Table to CSV: http://www.kunalbabre.com/projects/table2CSV.php (License unknown?)
